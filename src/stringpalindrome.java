1d7d82381c2115bff012b136c9177db3284cde0b
Dakschoudhary <dr0064@srmist.edu.in>
2026-02-18 14:55:28 +0530

    Strng palindrome

---
+++ src/stringpalindrome.java
@@ -0,0 +1,17 @@
+
+
+public class stringpalindrome {
+    public static boolean isPalindrome(String str) {
+        String cleanStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleanStr.length() - 1;
+        while (left < right) {
+            if (cleanStr.charAt(left) != cleanStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
