8422d8195e150e37050da22c3e4e49f7a23ccc39
DAKSH CHOUDHARY <dr0064@srmist.edu.in>
2026-03-05 13:12:00 +0530

    UC4

---
+++ UC4.java
@@ -0,0 +1,27 @@
+public class UC4{
+    public static void main(String[] args) {
+        String str = "madam";
+
+        char[] arr = str.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
